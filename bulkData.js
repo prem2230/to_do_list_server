18014191ab4a063d635357b7e9632061d12f0271
prem2230 <premcc6750@gmail.com>
2025-08-26 15:29:09 +0530

    Implement code changes to enhance functionality and improve performance

--- bulkData.js
+++ bulkData.js
@@ -1,6 +1,14 @@
 import fs from 'fs';
 
-const technologies = ["ReactJS", "ExpressJS", "JavaScript", "Docker", "NodeJS", "MongoDB", "TypeScript", "GraphQL", "Redis", "Kubernetes"];
+// const technologies = ["ReactJS", "ExpressJS", "JavaScript", "Docker", "NodeJS", "MongoDB", "TypeScript", "GraphQL", "Redis", "Kubernetes"];
+const technologies = ["ReactJS", "Vue.js", "Angular", "Svelte", "Next.js", "Nuxt.js", "Gatsby", "Ember.js", "Backbone.js", "jQuery", "ExpressJS", "NestJS", "Koa.js", "Fastify", "Hapi.js", "Django", "Flask", "FastAPI", "Spring Boot", "Laravel", "JavaScript", "TypeScript", "Python", "Java", "C#", "Go", "Rust", "PHP", "Ruby", "Kotlin",
+  "MongoDB", "PostgreSQL", "MySQL", "Redis", "Cassandra", "DynamoDB", "SQLite", "MariaDB", "CouchDB", "Neo4j",
+  "AWS", "Azure", "Google Cloud", "Heroku", "Vercel", "Netlify", "DigitalOcean", "Linode", "Firebase", "Supabase",
+  "Docker", "Kubernetes", "Jenkins", "GitLab CI", "GitHub Actions", "Terraform", "Ansible", "Vagrant", "Helm", "Istio",
+  "GraphQL", "REST API", "gRPC", "WebSocket", "Socket.io", "Apollo", "Prisma", "Hasura", "Strapi", "Postman",
+  "Jest", "Cypress", "Selenium", "Mocha", "Chai", "Puppeteer", "Playwright", "JUnit", "PyTest", "RSpec",
+  "Webpack", "Vite", "Parcel", "Rollup", "Gulp", "Grunt", "Babel", "ESLint", "Prettier", "Husky", "React Native", "Flutter", "Ionic", "Xamarin", "Swift", "Objective-C", "Android Studio", "Expo", "Cordova", "NativeScript"
+];
 const data = [];
 
 const now = new Date();
